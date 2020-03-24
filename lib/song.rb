59cd1ecb14edc8b05dc326313098fde1950d1be9
Heeraj Ramsanai <ramsanai_heeraj@live.com>
2020-03-24 22:17:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,6 +45,7 @@ class Song
   end
 
   def self.new_from_filename(filename)
+    song = self.new
     names = filename.split(' - ')
     song_name= names[1].gsub(".mp3","")
     artist_name= names[0]
